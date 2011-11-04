0724c08f6b4ba67955b037010e50b76f79283f04
Tatsuya Furukido <peco1103@gmail.com>
2011-11-05 05:43:09 +0900

    fix a few thing.

--- leonet_auth_improved.sh
+++ leonet_auth_improved.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# leonet認証スクリプト(improved)
+# レオネット認証スクリプト(improved)
 
 # filename : leonet_auth_improved.sh
-# date     : 8-April-2011
+# date     : 5-November-2011
 # author   : furu
 
 
@@ -13,28 +13,18 @@ PASS=
 SERVER="www.google.co.jp"
 PETTERN="^HTTP/1.1 200 OK"
 
-# mktemp -t : /tmp/ 以下にtmp.XXXXXXXXXXを作成
-# 下のじゃ動かない。tmplogfileには'mktep -t tmp.XXXXXXXXXX'が文字列で入る。
-# tmplogfile='mktemp -t tmp.XXXXXXXXXX' || exit 1
-touch tmp
+tmplogfile=$(mktemp /tmp/leonet_auth_tmp.XXXXXX) || exit 1
 
-# get http header for $SERVER
-curl -I ${SERVER} > tmp 2> /dev/null
+curl -I ${SERVER} > $tmplogfile 2> /dev/null
 
-grep "${PETTERN}" tmp > /dev/null 2>&1
-if [ ${?} -eq 0 ]
-then
-    echo "${SERVER} -> OK\n"
-    echo "Authentication has been done.\n"
+grep "${PETTERN}" $tmplogfile > /dev/null 2>&1
+if [ ${?} -eq 0 ]; then
+    echo "${SERVER} -> OK"
+    echo "Authentication has been done."
 else
-    echo "${SERVER} -> NG\n"
-    echo "start authentication ..."
-    wget --http-user=${ID} --http-password=${PASS} http://www.google.co.jp/ 2> /dev/null
+    echo "${SERVER} -> NG"
+    echo "Start authentication ..."
+    curl -o index.html -u ${ID}:${PASS} ${SERVER}/ 2> /dev/null
     rm index.html
-    echo "Authentication was completed!\n"
+    echo "Authentication was completed!"
 fi
-
-rm tmp
-
-# Wait.
-read uho
